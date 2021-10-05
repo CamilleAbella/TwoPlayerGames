dd1af90120dffa42008f53b2737e66993e2f5f88
CamilleAbella <camille.abella@hotmail.fr>
2021-10-05 14:20:17 +0200

    make possible to add more games

--- frontend/dist/games/clicker/script.js
+++ frontend/dist/games/clicker/script.js
@@ -1,20 +1,26 @@
 var socket = io()
 
+const game = "clicker"
+
+socket.emit(`newGame:${game}`)
+
 const button = document.getElementById("button")
 
 button.onclick = () => {
-  console.log("buttonClick")
-  socket.emit("buttonClick")
+  socket.emit(game, "buttonClick")
 }
 
-socket.on("buttonClick:ok", (ctx) => {
-  console.log("buttonClick:ok")
+socket.on(game, (event, ctx) => {
+  console.log("event", event)
 
-  let counter = document.getElementById(ctx.id) || document.createElement("div")
+  let counter = document.getElementById(ctx.id)
+
+  if (!counter) {
+    counter = document.createElement("div")
+    document.getElementById("counters").prepend(counter)
+  }
 
   counter.id = ctx.id
   counter.className = "counter"
   counter.innerHTML = String(ctx.clicks)
-
-  document.getElementById("counters").prepend(counter)
 })
